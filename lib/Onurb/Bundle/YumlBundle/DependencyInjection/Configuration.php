5684ba4cbe65bae678704eeee46ca8ea1b669db4
Nono1971 <herobrun@gmail.com>
2015-07-25 15:30:36 +0200

    fix insight troubles

--- lib/Onurb/Bundle/YumlBundle/DependencyInjection/Configuration.php
+++ lib/Onurb/Bundle/YumlBundle/DependencyInjection/Configuration.php
@@ -18,12 +18,6 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        //$rootNode = $treeBuilder->root('doctrine_yuml');
-
-        // Here you should define the parameters that are allowed to
-        // configure your bundle. See the documentation linked above for
-        // more information on that topic.
-
         return $treeBuilder;
     }
 }
